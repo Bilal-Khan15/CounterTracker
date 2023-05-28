64cd8e588954f090c0cb2fa6b935f466f478001f
Bilal Khan <bilalkhan@KHE-BILALKHAN-MAC.local>
2023-05-28 14:46:12 +0500

    Project Initialization

---
+++ src/main/java/com/ekar/countertracker/CounterTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ekar.countertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CounterTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ekar/countertracker/CounterTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekar.countertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
